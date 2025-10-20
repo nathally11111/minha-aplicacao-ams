cd4e328b845b9a9c1cffe4175ef3045fcec45366
nathally11111 <nathally3004@gmail.com>
2025-10-20 17:23:08 -0300

    Novo projeto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+     <h1>Site em React</h1>
+    </div>
+  );
+}
+
+export default App;
