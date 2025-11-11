8b21a8f59fc133bc2eca6b99f886368b525dce89
nathally11111 <nathally3004@gmail.com>
2025-11-11 15:03:38 -0300

    nav-bar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 import React from 'react';
 
-
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './css/App.css'
 import Contato from './Telas/Contato/Contato'
 import Produtos from './Telas/Produtos/Produtos'
 
 function App() {
  return (
     <Router>
-      <nav className="navbar navbar-expand-lg bg-dark " data-bs-theme="dark">
+        <nav className="navbar navbar-expand-lg navbar-purple">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Meu App</a>
+            <Link className="navbar-brand" to="/">Meu App</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                  <Link className="nav-link active" aria-current="page" to="/">Home</Link>
               </li>
               <li className="nav-item">
-                <Link to="/Produtos" className="nav-link">Produtos</Link>
+                  <Link to="/Produtos" className="nav-link">Produtos</Link>
               </li>
               <li className="nav-item">
-                <Link to="/Clientes" className="nav-link">Clientes</Link>
+                  <Link to="/Clientes" className="nav-link">Clientes</Link>
               </li>
               <li className="nav-item">
-                <Link to="/Vendas" className="nav-link">Vendas</Link>
+                  <Link to="/Vendas" className="nav-link">Vendas</Link>
               </li>
               <li className="nav-item">
-                <Link to="/Contato" className="nav-link">Contato</Link>
+                  <Link to="/Contato" className="nav-link">Contato</Link>
               </li>
             </ul>
           </div>
--- src/Telas/Produtos/Produtos.js
+++ src/Telas/Produtos/Produtos.js
@@ -1,49 +1,62 @@
 export default function Produtos(){
     return(
-    <div>
-        <h1>Página de Produtos</h1>
-<form>
-<label>Descrição do produto</label>
-<input name="nome"/>
-<br/>
-<label>Valor de Venda</label>
-<input name="valorVenda" type="number" step="0.01"/>
-<br/>
-<label>Código de barras</label>
-<input name="codigoBarras" type="text"/>
-<br/>
-<label>Estoque</label>
-<input name="estoque" type="number" />
-<br/>
-<button>Cadastrar Produto</button>
-</ form>
-<br/>
-<table>
-<thead>
-<tr>
-<th>Id</th>
-<th>Nome</th>
-<th>Valor de Venda</th>
-<th>Código de barras</th>
-<th>Quantidade em estoque</th>
-<th>Ações</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<td>1</td>
-<td>Pistão forjado</td>
-<td>R$ 152,30</td>
-<td>7896541238</td>
-<td>10</td>
-<td>
-<button>Editar</button>
-<button>Excluir</button>
-</td>
-</tr>
-</tbody>
-</table>
+    <div className="container-main">
+        <div className="card card-purple">
+            <div className="card-header">Página de Produtos</div>
+            <div className="card-body">
+                <form className="row g-3">
+                    <div className="col-md-6">
+                        <label className="form-label">Descrição do produto</label>
+                        <input className="form-control" name="nome" />
+                    </div>
+                    <div className="col-md-3">
+                        <label className="form-label">Valor de Venda</label>
+                        <input className="form-control" name="valorVenda" type="number" step="0.01" />
+                    </div>
+                    <div className="col-md-3">
+                        <label className="form-label">Estoque</label>
+                        <input className="form-control" name="estoque" type="number" />
+                    </div>
+                    <div className="col-md-6">
+                        <label className="form-label">Código de barras</label>
+                        <input className="form-control" name="codigoBarras" type="text" />
+                    </div>
+                    <div className="col-12">
+                        <button type="button" className="btn btn-primary custom">Cadastrar Produto</button>
+                    </div>
+                </form>
 
+                <hr/>
+
+                <div className="table-responsive">
+                    <table className="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Nome</th>
+                                <th>Valor de Venda</th>
+                                <th>Código de barras</th>
+                                <th>Quantidade em estoque</th>
+                                <th>Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Pistão forjado</td>
+                                <td>R$ 152,30</td>
+                                <td>7896541238</td>
+                                <td>10</td>
+                                <td>
+                                    <button className="btn btn-sm btn-outline-primary me-2">Editar</button>
+                                    <button className="btn btn-sm btn-outline-danger">Excluir</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>)
     
 }
